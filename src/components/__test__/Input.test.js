ae8d3d0e41750479c4b3dd57c9d7206571dfd864
Long Zhao <zlong@outlook.com>
2019-04-10 19:41:30 +1000

    Use prebuild instead of heroku-prebuild, fail a test to see result

--- src/components/__test__/Input.test.js
+++ src/components/__test__/Input.test.js
@@ -1,40 +1,40 @@
 import React from 'react';
-import { shallow , mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Input from '../Input';
 
 const props = {
-  label: "name",
-  value: "",
-  validationMessage:"Please enter your Name",
+  label: 'name',
+  value: '',
+  validationMessage: 'Please enter your Name',
   formDirty: true,
-  onChange: jest.fn()
+  onChange: jest.fn(),
 };
 
 let wrapper;
 
 describe('test Input component', () => {
   beforeEach(() => {
-    wrapper = mount(<Input {...props} />)
+    wrapper = mount(<Input {...props} />);
   });
 
-  it('should render the component to match snapshot',() => {
-    expect(wrapper).toMatchSnapshot()
+  it('should render the component to match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('should render correct label', () => {
-    expect(wrapper.find('label').text()).toEqual('name')
+    expect(wrapper.find('label').text()).toEqual('name1');
   });
 
   it('should receive correct props', () => {
-    expect(wrapper.props().formDirty).toBe(true)
+    expect(wrapper.props().formDirty).toBe(true);
   });
-  
+
   it('should render the validationMessage with given props', () => {
-    expect(wrapper.find('div').last().text()).toEqual('Please enter your Name')
+    expect(wrapper.find('div').last().text()).toEqual('Please enter your Name');
   });
 
   it('should trigger the correct function callback', () => {
-    wrapper.find('input').simulate('change')
-    expect(props.onChange).toHaveBeenCalledWith(wrapper.props().value)
+    wrapper.find('input').simulate('change');
+    expect(props.onChange).toHaveBeenCalledWith(wrapper.props().value);
   });
-})
+});
